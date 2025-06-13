cb24f875540c35c53d97850197da5afcb3eecc27
PrithviCode <114150857+PadmanabhK@users.noreply.github.com>
2025-06-13 16:59:54 +0530

    Update main.cpp

--- EasyProjects/FileEncryptDecrypt/main.cpp
+++ EasyProjects/FileEncryptDecrypt/main.cpp
@@ -4,8 +4,7 @@
 
 using namespace std;
 
-
-void ENCRYPT(string character, string word, string text, string result, int index, int shift){
+void CRYPT(string character, string word, string text, string result, int index, int shift, int choice){
     ifstream random("random.txt");
     while (getline(random, character)){
        text += character;
@@ -19,57 +18,123 @@ void ENCRYPT(string character, string word, string text, string result, int inde
             continue;
         }
         index = word.find((char)tolower(c));
-        if (index + shift > 25)
-        {
+        if (index + shift > 25 && choice == 1) {
             index = (index + shift) - 26;
             result += word[index];
             continue;
         }
-        result += word[index + shift];
-    }
-    ofstream write("random.txt");
-    write << result;
-    write.close();
-}
-
-
-void DECRYPT(string character, string word, string text, string result, int index, int shift){
-    ifstream random("random.txt");
-    while (getline(random, character))
-    {   text += character;
-
-    }
-    random.close();
-    for (char c : text)
-    {
-        if(c == ' ' || c == '.' || c == ','){
-            result += c;
+        if (index - shift < 0 && choice == 2) {
+            index = 26 + index - shift;
+            result += word[index];
             continue;
         }
-        index = word.find((char)tolower(c));
-        if (index - shift < 0) {
-            result += word[26 + index - shift];
-        }
-        else{
-            result += word[index - shift];
-        }
+        result += (choice == 1) ? word[index + shift] : word[index - shift];
     }
+
     ofstream write("random.txt");
     write << result;
     write.close();
 }
-
 
 int main(){
     string word = "abcdefghijklmnopqrstuvwxyz";
-    string character, text, result, re_result;
+    string character, text, result;
     int index{0};
-    int shift{8};
-    ENCRYPT(character, word, text, result, index, shift);
+    int shift{5};
+    CRYPT(character, word, text, result, index, shift, 1);
     character = "", text = "", result = "", index = 0;
-    DECRYPT(character, word, text, result, index, shift);
+    CRYPT(character, word, text, result, index, shift, 2);
     return 0;
 }
+
+
+
+
+
+
+
+// #include <cctype>
+// #include <fstream>
+// #include <string>
+
+// using namespace std;
+
+
+// void ENCRYPT(string character, string word, string text, string result, int index, int shift){
+//     ifstream random("random.txt");
+//     while (getline(random, character)){
+//        text += character;
+//     }
+//     random.close();
+
+//     for (char c : text)
+//     {
+//         if(c == ' ' || c == '.' || c == ','){
+//             result += c;
+//             continue;
+//         }
+//         index = word.find((char)tolower(c));
+//         if (index + shift > 25)
+//         {
+//             index = (index + shift) - 26;
+//             result += word[index];
+//             continue;
+//         }
+//         result += word[index + shift];
+//     }
+//     ofstream write("random.txt");
+//     write << result;
+//     write.close();
+// }
+
+
+// void DECRYPT(string character, string word, string text, string result, int index, int shift){
+//     ifstream random("random.txt");
+//     while (getline(random, character))
+//     {   text += character;
+
+//     }
+//     random.close();
+//     for (char c : text)
+//     {
+//         if(c == ' ' || c == '.' || c == ','){
+//             result += c;
+//             continue;
+//         }
+//         index = word.find((char)tolower(c));
+//         if (index - shift < 0) {
+//             result += word[26 + index - shift];
+//         }
+//         else{
+//             result += word[index - shift];
+//         }
+//     }
+//     ofstream write("random.txt");
+//     write << result;
+//     write.close();
+// }
+
+
+// int main(){
+//     string word = "abcdefghijklmnopqrstuvwxyz";
+//     string character, text, result, re_result;
+//     int index{0};
+//     int shift{8};
+//     ENCRYPT(character, word, text, result, index, shift);
+//     character = "", text = "", result = "", index = 0;
+//     DECRYPT(character, word, text, result, index, shift);
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
 
 
 
