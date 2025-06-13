218bf53944cfc3246f861eeaf643ffca1cacb4b5
PrithviCode <114150857+PadmanabhK@users.noreply.github.com>
2025-06-13 16:03:07 +0530

    Update main.cpp

--- EasyProjects/FileEncryptDecrypt/main.cpp
+++ EasyProjects/FileEncryptDecrypt/main.cpp
@@ -1,38 +1,72 @@
-#include <iostream>
+#include <cctype>
 #include <fstream>
+#include <string>
 
 using namespace std;
+
+
+void ENCRYPT(string character, string word, string text, string result, int index, int shift){
+    ifstream random("random.txt");
+    while (getline(random, character)){
+       text += character;
+    }
+    random.close();
+
+    for (char c : text)
+    {
+        if(c == ' ' || c == '.' || c == ','){
+            result += c;
+            continue;
+        }
+        index = word.find((char)tolower(c));
+        if (index + shift > 25)
+        {
+            index = (index + shift) - 26;
+            result += word[index];
+            continue;
+        }
+        result += word[index + shift];
+    }
+    ofstream write("random.txt");
+    write << result;
+    write.close();
+}
+
+
+void DECRYPT(string character, string word, string text, string result, int index, int shift){
+    ifstream random("random.txt");
+    while (getline(random, character))
+    {   text += character;
+
+    }
+    random.close();
+    for (char c : text)
+    {
+        if(c == ' ' || c == '.' || c == ','){
+            result += c;
+            continue;
+        }
+        index = word.find((char)tolower(c));
+        if (index - shift < 0) {
+            result += word[26 + index - shift];
+        }
+        else{
+            result += word[index - shift];
+        }
+    }
+    ofstream write("random.txt");
+    write << result;
+    write.close();
+}
+
 
 int main(){
     string word = "abcdefghijklmnopqrstuvwxyz";
     string character, text, result, re_result;
     int index{0};
-    int shift{5};
-    // ifstream random("random.txt");
-    // while (getline(random, character))
-    // {   text += character;
-        
-    // }
-    // random.close();
-    
-    for (char c : word)
-    {
-        index = word.find(c);
-        if (index + shift > 25)
-        {
-            index = (index + shift) - 26;
-            // cout << c << " - EXCEEDED!!! - " << index << " - " << word[index] << endl;
-            // cout << c << " - " << word[index] << endl;
-            result += word[index];
-            continue;
-        }
-        
-        // cout << c << " - " << word[index + shift] << endl;
-        result += word[index + shift];
-    }
-    
-    cout << result << endl;
-
+    int shift{8};
+    ENCRYPT(character, word, text, result, index, shift);
+    character = "", text = "", result = "", index = 0;
+    DECRYPT(character, word, text, result, index, shift);
     return 0;
 }
-
